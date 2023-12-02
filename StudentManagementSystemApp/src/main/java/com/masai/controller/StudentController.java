3043b398b0b5e98821a73f5b9d0d73fddcd637fa
salman <sk752547@gmail.com>
2023-12-02 16:04:48 +0530

    put to pathc

--- StudentManagementSystemApp/src/main/java/com/masai/controller/StudentController.java
+++ StudentManagementSystemApp/src/main/java/com/masai/controller/StudentController.java
@@ -8,9 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,7 +51,7 @@ public class StudentController {
 		return new ResponseEntity<>(studentList,HttpStatus.OK);
 	}
 	
-	@PutMapping("/students/{rollNo}")
+	@PatchMapping("/students/{rollNo}")
 	public ResponseEntity<Student> updateStudent(@RequestBody @Valid StudentDto studentdto,@PathVariable("rollNo") Integer rollno){
 		 
 	 Student stud = studentService.updateStudentDetails(studentdto, rollno);
